69a51ae5a3d3d49f91b8f1835e59ff211b96600b
Ahmed <dropheartyt@gmail.com>
2020-10-12 16:17:21 +0300

    updated eval.js to catch an error instead of suiciding at the sight of bad code

--- commands/eval.js
+++ commands/eval.js
@@ -3,5 +3,10 @@ exports.run = (client, message, args) => {
     if (message.author.id !== client.config.ownerid) return;
     code = args.join(' ')
     console.log(code)
-    message.channel.send(eval(code), {code:"xl"}); 
+    try {
+        evald = eval(code)
+        message.channel.send(evald, {code:"xl"}); 
+    } catch {
+        message.channel.send("dude your code sucks")
+    }
 }
