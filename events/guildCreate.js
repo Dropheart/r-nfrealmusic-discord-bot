e35ca4f87f3d392decf3274d14cba76abb075b81
Ahmed <dropheartyt@gmail.com>
2020-10-17 19:08:26 +0300

    changed to ymls

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,9 +1,7 @@
 const fs = require('fs')
 
-
-
 module.exports = (client, guild) => {
-    fs.open(`./servers/${guild.id}.json`, 'w', function(err) {
+    fs.open(`./servers/${guild.id}.yml`, 'w', function(err) {
         if(err) console.log(err);
         console.log(`Joined ${guild.name} / ${guild.id}`)
     })
