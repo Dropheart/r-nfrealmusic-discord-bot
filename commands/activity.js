1c7b55f6f35a8b139e337b5d61c51166c797f6c4
Ahmed <dropheartyt@gmail.com>
2020-10-17 05:47:02 +0300

    activity.js now lets u set watching too which is cool

--- commands/activity.js
+++ commands/activity.js
@@ -1,7 +1,13 @@
 exports.run = (client, message, args) => {
     if (message.author.id !== client.config.ownerid) return;
-    command = '!activity'
-    tobeset = String(message.content.slice(command.length))
-    client.user.setActivity(tobeset);
-    message.channel.send("Activity has been set to `" + tobeset + "`.") 
+    activity = args.join(' ').split(' - ')
+    try {
+    console.log(activity[0], activity[1].toUpperCase())
+    activitytype = activity[1].toUpperCase()
+    client.user.setActivity(activity[0], { type: String(activitytype) });
+    message.channel.send("Activity has been set to `" + activity[0] + "`.") 
+    } catch {
+        client.user.setActivity(activity[0])
+        message.channel.send("Activity has been set to `" + activity[0] + "`.") 
+    }
 }
