c8145cde40196a38d00459fee24c3f73af060390
Ahmed <dropheartyt@gmail.com>
2020-10-21 12:00:22 +0300

    added avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,25 @@
+const { User } = require("discord.js")
+
+exports.run = (client, message, args) => {
+    if (!args[0]) {
+        message.channel.send(message.author.displayAvatarURL())
+    } else {
+        try {
+            theirid = args[0]
+            if (args[0]) {
+                if (typeof(args[0]) == 'number') {
+                    args[0] = theirid
+                    console.log(theirid)
+                } else if (args[0].startsWith('<@!')) {
+                    theirid = args[0].slice(3, -1)
+                } else if (args[0].startsWith('<@')) {
+                    theirid = args[0].slice(2, -1)
+                }
+            }
+            person = client.users.cache.get(theirid)
+            message.channel.send(person.displayAvatarURL())
+    } catch (err) {
+        console.log(err)
+        }
+    }
+}
