f7951f26cb2fe16d4f360c3c798172a7a6fc8e83
Ahmed <dropheartyt@gmail.com>
2020-10-21 16:46:45 +0300

    removed arg which apparently breaks it. fixed.

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -2,7 +2,7 @@ const yml = require('yaml')
 const fs = require('fs')
 const { client } = require('../bot.js')
 
-module.exports = (oldMessage, newMessage) => {
+module.exports = (message, oldMessage, newMessage) => {
     // see if config exists, if not, exit
     if (!fs.existsSync(`./servers/${oldMessage.guild.id}.yml`)) return;
     try { var fsread = fs.readFileSync(`./servers/${oldMessage.guild.id}.yml`, 'utf8')
@@ -15,7 +15,7 @@ module.exports = (oldMessage, newMessage) => {
     client.channels.fetch(logchannel).then(
     console.log(client.channels.cache.get(logchannel),
     client.channels.cache.get(logchannel).send(
-        `✏ **${oldMessage.author.username}#${oldMessage.author.discriminator}** (${oldMessage.author.id} / <@${oldMessage.author.id}>) edited their message at **${d}** in <#${oldMessage.channel.id}> (**${oldMessage.channel.name}**, ${oldMessage.channel.id}) \n \`\`\`${oldMessage.content}\`\`\` \`to\` \`\`\`${newMessage.content}\`\`\` \n `
+        `✏ **${oldMessage.author.username}#${oldMessage.author.discriminator}** (${oldMessage.author.id} / <@${oldMessage.author.id}>) edited their message at **${d}** in <#${oldMessage.channel.id}> (**${oldMessage.channel.name}**, ${oldMessage.channel.id}) \n \`\`\`${oldMessage.content}\`\`\` \`to\` \`\`\`${newMessage.content}\`\`\` \n `, {"allowedMentions": { "users" : []}}
     )))
     } catch (err) {
         console.log(err)
