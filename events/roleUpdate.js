b7d46d37fd1ab5310bdb706e94eb22d16557a1ff
Ahmed <dropheartyt@gmail.com>
2020-10-22 23:52:07 +0300

    fixed a bit in roleupdate.js

---
+++ events/roleUpdate.js
@@ -0,0 +1,5 @@
+const log = require("../functions/log.js")
+
+module.exports = (client, oldRole, newRole) => {
+    log(client, `❗ **Role has been updated at {d}**\n Permission bitfield: ${oldRole.permissions.bitfield} --> ${newRole.permissions.bitfield}\n Role Name: ${oldRole.name} --> ${newRole.name}`, oldRole)
+}
