13a932b029fc0897e1aa30a5d1064448279ddbfa
Ahmed <dropheartyt@gmail.com>
2020-10-22 22:39:12 +0300

    added func instead of repeating code

---
+++ functions/log.js
@@ -0,0 +1,19 @@
+const yml = require('yaml')
+const fs = require('fs')
+
+
+module.exports = (client, logmessage, logged) => {
+    if (!fs.existsSync(`./servers/${logged.guild.id}.yml`)) return;
+    try { var fsread = fs.readFileSync(`./servers/${logged.guild.id}.yml`, 'utf8')
+        var serverconf = yml.parseDocument(fsread).toJSON()
+        if (!serverconf.logchannels.messagelogs[0]) return;
+        var logchannel = serverconf.logchannels.messagelogs[0]
+        console.log(client.channels.fetch(logchannel))
+        var d = new Date()
+        client.channels.fetch(logchannel).then(
+        console.log(client.channels.cache.get(logchannel),
+        client.channels.cache.get(logchannel).send(logmessage.replace('{d}', d), {"allowedMentions": { "users" : []}} )))
+    } catch (err) {
+        console.log(err)
+    }
+}
