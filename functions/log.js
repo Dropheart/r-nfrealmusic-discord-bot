eb319c05397e7399e28f33c2d46fbc015b56c203
Ahmed <dropheartyt@gmail.com>
2020-10-23 00:46:31 +0300

    removed some logging to clean terminal

--- functions/log.js
+++ functions/log.js
@@ -9,11 +9,9 @@ module.exports = (client, logmessage, logged) => {
         var serverconf = yml.parseDocument(fsread).toJSON()
         if (!serverconf.logchannels.messagelogs[0]) return;
         var logchannel = serverconf.logchannels.messagelogs[0]
-        console.log(client.channels.fetch(logchannel))
         var d = new Date()
         client.channels.fetch(logchannel).then(
-        console.log(client.channels.cache.get(logchannel),
-        client.channels.cache.get(logchannel).send(logmessage.replace('{d}', d), {"allowedMentions": { "users" : []}} )))
+        client.channels.cache.get(logchannel).send(logmessage.replace('{d}', d), {"allowedMentions": { "users" : []}} ))
     } catch (err) {
         console.log(err)
     }
