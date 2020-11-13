a2213eaecca949db0af1beed95c8302bcf7e0d56
Ahmed <dropheartyt@gmail.com>
2020-11-13 04:53:35 +0300

    command to check your permission level

---
+++ commands/permissionlevel.js
@@ -0,0 +1,6 @@
+const permcheck= require('../functions/permissioncheck.js')
+
+
+exports.run = (client, message, args) => {
+    message.channel.send(`Your permission level is: **${permcheck(client, message)}**`)
+}
