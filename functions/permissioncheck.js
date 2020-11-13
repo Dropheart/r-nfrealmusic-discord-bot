9aabd3a4d1310dfea1aa22b98480e80277b835b7
Ahmed <dropheartyt@gmail.com>
2020-11-13 04:54:42 +0300

    function to check & pass permission lvl & boolean

---
+++ functions/permissioncheck.js
@@ -0,0 +1,40 @@
+const yml = require('yaml')
+const fs = require('fs')
+
+
+module.exports = (client, message, command) => {
+    
+    if (message.channel && message.channel.type == "dm") return;
+    if (!fs.existsSync(`./servers/${message.guild.id}.yml`)) return;
+    try { var fsread = fs.readFileSync(`./servers/${message.guild.id}.yml`, 'utf8')
+        var serverconf = yml.parseDocument(fsread).toJSON()
+        var roles = message.member.roles.cache.keyArray()
+        console.log(message.member.roles.cache.keyArray())
+        var perms = 0
+        console.log(Object.entries(serverconf.rolePermissions))
+        for (var [key, value] of Object.entries(serverconf.rolePermissions)) {
+            console.log(`${key} and ${value}`)
+            console.log(roles.includes(key))
+            if (roles.includes(key) && value > perms) {
+                var perms = value
+            }
+        }
+    } catch (err) {
+        console.log(err)
+    }
+
+    if (command !== null) {
+        if (!serverconf.commandPermissions) return;
+        var permLevel = serverconf.commandPermissions[command]
+        console.log(permLevel)
+    }
+
+    if (!command) {
+        return(perms)
+    } else if (permLevel <= perms) {
+        return(true)
+    } else {
+        return(false)
+    }
+
+}
