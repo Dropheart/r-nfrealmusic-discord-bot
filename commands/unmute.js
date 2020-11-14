c0841f5527b427d9a7754d9f7fa6ffcdb1ea32ee
Ahmed <dropheartyt@gmail.com>
2020-11-14 05:44:48 +0300

    can't unmute ppl of same level

--- commands/unmute.js
+++ commands/unmute.js
@@ -4,8 +4,15 @@ const fs = require('fs')
 
 
 exports.run = (client, message, args) => {
-    var permission = permcheck(client, message, 'mute')
+    var permission = permcheck(client, message, message.member, 'mute')
     if (!permission) return;
+
+    var uid = args[0]
+    args = args.splice(1)
+    if (permcheck(client, message, message.guild.member(uid)) >= permcheck(client, message, message.member)) {
+        message.channel.send("🚫 You do not have permission to unmute that user.")
+        return;
+    }
 
     try { var fsread = fs.readFileSync(`./servers/${message.guild.id}.yml`, 'utf8')
     var serverconf = yml.parseDocument(fsread).toJSON()
@@ -15,8 +22,6 @@ exports.run = (client, message, args) => {
     }
 
     try {
-        var uid = args[0]
-        args = args.splice(1)
         mutereason = args.join(' ')
         member = message.guild.member(uid).user.tag 
         message.guild.member(uid).roles.remove(muterole, mutereason).then(message.channel.send(`User **${member}** has been unmuted.`))
