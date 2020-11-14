70eaa3c27c77de6e35e388da5a4289f76bd33ca8
Ahmed <dropheartyt@gmail.com>
2020-11-14 05:45:11 +0300

    specify user to perm check, console logs commented

--- functions/permissioncheck.js
+++ functions/permissioncheck.js
@@ -2,19 +2,19 @@ const yml = require('yaml')
 const fs = require('fs')
 
 
-module.exports = (client, message, command) => {
+module.exports = (client, message, user, command) => {
     
     if (message.channel && message.channel.type == "dm") return;
     if (!fs.existsSync(`./servers/${message.guild.id}.yml`)) return;
     try { var fsread = fs.readFileSync(`./servers/${message.guild.id}.yml`, 'utf8')
         var serverconf = yml.parseDocument(fsread).toJSON()
-        var roles = message.member.roles.cache.keyArray()
-        console.log(message.member.roles.cache.keyArray())
+        var roles = user.roles.cache.keyArray()
+        // console.log(user.roles.cache.keyArray())
         var perms = 0
-        console.log(Object.entries(serverconf.rolePermissions))
+        // console.log(Object.entries(serverconf.rolePermissions))
         for (var [key, value] of Object.entries(serverconf.rolePermissions)) {
-            console.log(`${key} and ${value}`)
-            console.log(roles.includes(key))
+            // console.log(`${key} and ${value}`)
+            // console.log(roles.includes(key))
             if (roles.includes(key) && value > perms) {
                 var perms = value
             }
@@ -26,7 +26,7 @@ module.exports = (client, message, command) => {
     if (command !== null) {
         if (!serverconf.commandPermissions) return;
         var permLevel = serverconf.commandPermissions[command]
-        console.log(permLevel)
+        // console.log(permLevel)
     }
 
     if (!command) {
