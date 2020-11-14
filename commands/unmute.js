959c65f9e277410b3396723a27319a779279e61b
Ahmed <dropheartyt@gmail.com>
2020-11-14 21:13:56 +0300

    better errors, rmv mute from yml

--- commands/unmute.js
+++ commands/unmute.js
@@ -7,23 +7,39 @@ exports.run = (client, message, args) => {
     var permission = permcheck(client, message, message.member, 'mute')
     if (!permission) return;
 
-    var uid = args[0]
-    args = args.splice(1)
-    if (permcheck(client, message, message.guild.member(uid)) >= permcheck(client, message, message.member)) {
-        message.channel.send("🚫 You do not have permission to unmute that user.")
+    
+    try { var fsread = fs.readFileSync(`./servers/${message.guild.id}.yml`, 'utf8')
+        var serverconf = yml.parseDocument(fsread).toJSON()
+        var muterole = serverconf.muteRole
+    } catch (err) {
+        message.channel.send("Configure a mute role.")
         return;
     }
 
-    try { var fsread = fs.readFileSync(`./servers/${message.guild.id}.yml`, 'utf8')
-    var serverconf = yml.parseDocument(fsread).toJSON()
-    var muterole = serverconf.muteRole
+    try {
+        var uid = args[0]
+        if (!message.guild.member(uid).roles.cache.keyArray().includes(muterole)) {
+            message.channel.send(`**${message.guild.member(uid).user.tag}** is already unmuted.`)
+            return;
+        }
+        args = args.splice(1)
+        if (permcheck(client, message, message.guild.member(uid)) >= permcheck(client, message, message.member)) {
+            message.channel.send("🚫 You do not have permission to unmute that user.")
+            return;
+        }
     } catch (err) {
-        console.log(err)
+        message.channel.send("Please supply a user ID.")
+        return;
     }
 
     try {
         mutereason = args.join(' ')
-        member = message.guild.member(uid).user.tag 
+        member = message.guild.member(uid).user.tag
+        let fsread = fs.readFileSync(`./timers/mutes/${message.guild.id}.yml`, 'utf8')
+        let regex = new RegExp("'" + uid + "': \\d{10,11}\\n", "g") 
+        let newData = fsread.replace(regex, '')
+        fs.writeFileSync(`./timers/mutes/${message.guild.id}.yml`, newData, 'utf8')
+        
         message.guild.member(uid).roles.remove(muterole, mutereason).then(message.channel.send(`User **${member}** has been unmuted.`))
     } catch(err) {
         console.log(err)
