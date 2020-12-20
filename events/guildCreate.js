31821f9d61bd6b5ab47023965abd0fb6fe35e296
Ahmed <dropheartyt@gmail.com>
2020-12-20 07:59:56 +0300

    create infraction table in db

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,4 +1,6 @@
 const fs = require('fs')
+const sql = require('../mariadb.js')
+const config = require('../config.json')
 
 module.exports = (client, guild) => {
     fs.open(`./servers/${guild.id}.yml`, 'w', function(err) {
@@ -8,5 +10,15 @@ module.exports = (client, guild) => {
     fs.open(`./timers/mutes/${guild.id}.yml`, 'w', function(err) {
         if(err) console.log(err);
     })
+    
+    async function f() {
+        try {
+            res = await sql.query(`CREATE TABLE ${guild.id} (caseid INT NOT NULL, reason TEXT NULL DEFAULT NULL, victim BIGINT NULL DEFAULT NULL, moderator BIGINT NULL DEFAULT NULL, date INT NULL DEFAULT NULL, type TINYTEXT NULL DEFAULT NULL, link TINYTEXT NULL DEFAULT NULL);`)
+        } catch (err) {
+            console.log(err)
+        } 
+    }
+    
+    f()
 
 }
