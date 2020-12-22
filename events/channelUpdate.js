2ec07da3534f711f9f73b1c01b6f0909ac90dcac
Ahmed // Dropheart <ahmed@techfromtheheart.com>
2020-12-22 07:36:09 +0300

    not freak out on position change

--- events/channelUpdate.js
+++ events/channelUpdate.js
@@ -1,6 +1,7 @@
 const log = require('../functions/log.js')
 
 module.exports = (client, oldChannel, newChannel) => {
+    if (oldChannel.position != newChannel.position || oldChannel.rawPosition != newChannel.rawPosition) return;
     if (!oldChannel.topic) {var oldtopic = "[ No topic ]"} else {oldtopic = oldChannel.topic}
     if (!newChannel.topic) {var newtopic = "[ No topic ]"} else {newtopic = newChannel.topic}
     log(client, `🔃 **Channel ${newChannel} has been updated**. For more information, check the audit logs.\n 
