f3eb6806fcbb4770ff7b858098a5af26f23626dc
Ahmed // Dropheart <ahmed@techfromtheheart.com>
2020-12-22 09:08:40 +0300

    i did nothing and it broke

--- commands/permissionlevel.js
+++ commands/permissionlevel.js
@@ -2,5 +2,5 @@ const permcheck= require('../functions/permissioncheck.js')
 
 
 exports.run = (client, message, args) => {
-    message.channel.send(`Your permission level is: **${permcheck(client, message)}**`)
+    message.channel.send(`Your permission level is: **${permcheck(client, message, message.member)}**`)
 }
