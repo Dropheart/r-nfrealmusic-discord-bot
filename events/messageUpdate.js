1b829467739ab51ce7868405e5c53ddac27eee4d
Ahmed // Dropheart <ahmed@techfromtheheart.com>
2020-12-22 07:35:41 +0300

    not freak out on an embed

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,5 +1,6 @@
 const log = require('../functions/log.js')
 
 module.exports = (client, oldMessage, newMessage) => {
-    log(client, `✏ **${oldMessage.author.username}#${oldMessage.author.discriminator}** (${oldMessage.author.id} / <@${oldMessage.author.id}>) edited their message at **{d}** in <#${oldMessage.channel.id}> (**${oldMessage.channel.name}**, ${oldMessage.channel.id}) \`\`\`${oldMessage.content}\`\`\` to \`\`\`${newMessage.content}\`\`\` \n `, oldMessage)
+    if (oldMessage.content == newMessage.content && oldMessage.embeds != newMessage.embeds) return;
+    log(client, `✏ **${oldMessage.author.username}#${oldMessage.author.discriminator}** (${oldMessage.author.id} / <@${oldMessage.author.id}>) edited their message (${oldMessage.id}) at **{d}** in <#${oldMessage.channel.id}> (**${oldMessage.channel.name}**, ${oldMessage.channel.id}) \`\`\`${oldMessage.content}\`\`\` to \`\`\`${newMessage.content}\`\`\` \n `, oldMessage)
 }
