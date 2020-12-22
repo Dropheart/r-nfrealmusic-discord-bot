f08b307991853a9c5a080df1f1f187d640ad3a46
Ahmed // Dropheart <ahmed@techfromtheheart.com>
2020-12-22 07:35:50 +0300

    incl message id

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,5 +1,5 @@
 const log  = require('../functions/log.js')
 
 module.exports = (client, message) => {
-    log(client, `🗑 **${message.author.username}#${message.author.discriminator}** (${message.author.id} / <@${message.author.id}>) deleted their message at **{d}** in <#${message.channel.id}> (**${message.channel.name}**, ${message.channel.id}) \`\`\`${message.content}\`\`\` `, message)
+    log(client, `🗑 **${message.author.username}#${message.author.discriminator}** (${message.author.id} / <@${message.author.id}>) deleted their message (${message.id}) at **{d}** in <#${message.channel.id}> (**${message.channel.name}**, ${message.channel.id}) \`\`\`${message.content}\`\`\` `, message)
 }
