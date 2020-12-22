107e7376536a2107a1cf5691b04f123ca5fdfd39
Ahmed // Dropheart <ahmed@techfromtheheart.com>
2020-12-22 07:37:08 +0300

    add note on user

---
+++ commands/note.js
@@ -0,0 +1,26 @@
+const permcheck= require('../functions/permissioncheck.js')
+const modlog = require('../functions/modlog.js')
+
+
+exports.run = async (client, message, args) => {
+    var permission = permcheck(client, message, message.member, 'note')
+    if (!permission) return;
+
+    let uid
+    uid = args[0]
+    args = args.splice(1)
+    console.log(args)
+    myguy = await client.users.fetch(uid)
+    
+    let reason = args.join(' ')
+
+    try {
+        cid = await modlog(client, message, 'Note', uid, reason)
+        message.channel.send(`‼ ${cid[0]}Added a note on **${myguy.tag}**. (Case ${cid[1]})`)
+    } catch (err) {
+        console.log(err)
+        console.log(err.messaage)
+        return;
+    }
+
+}
