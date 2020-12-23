96a79c42a60c9e61f6617e9f57a396190db5ec13
Ahmed // Dropheart <ahmed@techfromtheheart.com>
2020-12-23 06:21:28 +0300

    get permlevel of someone else

--- commands/permissionlevel.js
+++ commands/permissionlevel.js
@@ -1,6 +1,15 @@
 const permcheck= require('../functions/permissioncheck.js')
+const getuid = require('../functions/getuid.js')
 
 
-exports.run = (client, message, args) => {
-    message.channel.send(`Your permission level is: **${permcheck(client, message, message.member)}**`)
+exports.run = async (client, message, args) => {
+    if (args[0]) {
+        uid = getuid(message, args)
+        name = await client.users.fetch(uid)
+        gmember = await message.guild.member(uid) 
+        message.channel.send(`**${name.tag}**'s permission level is: **${permcheck(client, message, gmember)}**`)
+    } else {
+        message.channel.send(`Your permission level is: **${permcheck(client, message, message.member)}**`)
+    }
+
 }
