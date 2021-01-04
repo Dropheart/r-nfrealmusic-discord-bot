9b6ed1a023f4c6501fd668ec1b0e9f39fe53ea34
Ahmed // Dropheart <ahmed@techfromtheheart.com>
2021-01-04 08:44:38 +0300

    ratelimit to stop two cases having the same number

---
+++ functions/ratelimits.js
@@ -0,0 +1,18 @@
+ratelimits = new Array()
+
+module.exports = (message, truthy, type) => {
+    if (type == 'mod') {
+        if (truthy) {
+            ratelimits.push(message.author.id)
+        } else {
+            ratelimits.pop(message.author.id)
+        }
+    }
+
+    if (ratelimits.includes(message.author.id)) {
+        ratelimited = true
+    } else {
+        ratelimited = false
+    }
+    return ratelimited;
+}
