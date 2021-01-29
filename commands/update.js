d79a8572ff4490330b7c537ceef9bd679aad81e8
Ahmed // Dropheart <ahmed@techfromtheheart.com>
2021-01-29 06:31:42 +0300

    PROPER CASE UPDAATING

--- commands/update.js
+++ commands/update.js
@@ -7,7 +7,7 @@ exports.run = async (client, message, args) => {
     if (!permission) return;
 
     caseid = args[0]
-    let things = await sql.query(`SELECT caseid, reason, victim, moderator, date, type, link FROM \`${message.guild.id}\` WHERE caseid=${caseid}`)
+    let things = await sql.query(`SELECT caseid, reason, updates, victim, moderator, date, type, link FROM \`${message.guild.id}\` WHERE caseid=${caseid}`)
     if (!things[0]) {
         message.channel.send('❓ No case exists with this number.')
         return;
@@ -17,57 +17,48 @@ exports.run = async (client, message, args) => {
         message.channel.send('❓ Please give me something to update the case with.')
         return;
     }
-    let reason = things[0].reason
+
     regex = things[0].link.match(/([0-9]+)/g)
     msgid = regex[2]
     logchannel = regex[1]
 
     tbr = args.splice(1).join(' ')
-    reason = `${reason}\n\n[Update] from **${message.author.tag}** @ **${new Date()}**:\n ${tbr}`
-    await sql.query(`UPDATE \`${message.guild.id}\` SET reason = '${reason}' WHERE caseid=${caseid}`)
-
+    if (things[0].updates) {
+        console.log(things[0].updates)
+        pleasework = things[0].updates
+        pleasework.push(`${tbr}`)
+        pleasework = JSON.stringify(pleasework)
+    } else {
+        pleasework = [`${tbr}`]
+        pleasework = JSON.stringify(pleasework)
+    }
     try { 
         chanl = await client.channels.fetch(logchannel) 
         mesag = await client.channels.cache.get(logchannel).messages.fetch(msgid)
     } catch (err) {
         console.log(err)
     }
-
-
-    if (things[0].type === 'Mute') {
-        color = '8B0000'
-    } else if (things[0].type === 'Ban') {
-        color = 'FF0000'
-    } else if (things[0].type === 'Unmute') {
-        color = '00FF00'
-    } else if (things[0].type === 'Unban') {
-        color = '00FF00'
-    } else if (things[0].type === 'Kick') {
-        color = 'FFA500'
-    } else if (things[0].type === 'Warn') {
-        color = '4A0D00'
-    } else if (things[0].type === 'Note') {
-        color = '00EAFF'
-    }
-
-    let myguy = await client.users.fetch(things[0].victim)
-    let mod = await client.users.fetch(things[0].moderator)
-    let d = new Date(things[0].date * 1000)
-
-    embed = new MessageEmbed()
-    .setTitle(`${things[0].type} - Case #${things[0].caseid}`)
-    .setColor(color)
-    .addField("Victim", `${myguy.tag}\n<@${myguy.id}>`, true)
-    .addField("Moderator", `${mod.tag}\n<@${mod.id}>`, true)
-    .addField(`Reason`, reason, false)
-    .addField(`Time`, d, false)
+    
+    ogembed = await mesag.embeds[0]
+    embed = mesag.embeds[0]
+    embed.addField(`${message.author.tag} @ ${new Date()}`, tbr, false)
 
     try {
         mesag.edit(embed)
     } catch (err) {
         console.log(err)
+        message.channel.send(`I cannot update the embed and thus cannot update the case. Ensure that I have permission to send messages in <#${logchannel}>, and that your updates aren't too long.`)
+        return;
     }
 
+    try {
+        await sql.query(`UPDATE \`${message.guild.id}\` SET updates = '${pleasework}' WHERE caseid=${caseid}`)
+    } catch (err) {
+        mesag.edit(ogembed)
+        console.log(err)
+        return;
+    }
     message.channel.send('✔ Case has been updated.')
 
 }
+
